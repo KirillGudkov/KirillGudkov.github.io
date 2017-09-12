eb129239598c117ef87521cf460aec544d8383aa
Gudkov Kirill <for.example.nickname@gmail.com>
2017-09-13 00:58:10 +0300

    change first section and add some effects

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('#fullpage').fullpage({
-		scrollingSpeed: 600,
+		scrollingSpeed: 500,
 		autoScrolling: true,
 	});
 });
@@ -11,3 +11,27 @@ $(window).on('load', function () {
     $loader.fadeOut();
     $preloader.delay(350).fadeOut('slow');
   });
+
+// when user click on "contact" block with contact form appear and layer under is blured
+$("#contact").click(function(){
+	$(".contact-container").fadeIn(300);
+	$(".form").animate({'margin-left': '25%'}, 400);
+	$("#fullpage").addClass('blured');
+	$("#fullpage").removeClass('non-blured');
+});
+
+//button to close contact from and make layer under no blured
+$(".close-button").click(function(){
+	$(".contact-container").fadeOut(300);
+	$(".form").animate({'margin-left': '45%'}, 400);
+	$("#fullpage").removeClass('blured');
+	$("#fullpage").addClass('non-blured');
+});
+
+$(".scroll-down").click(function(){
+	$.fn.fullpage.moveSectionDown();
+});
+
+$("#works").click(function(){
+	$.fn.fullpage.moveTo(4);
+});
